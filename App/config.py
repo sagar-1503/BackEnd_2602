c774914c5d2c7ad77012a3f6da2fa70c8bbcbe7d
sagar-1503 <161993744+sagar-1503@users.noreply.github.com>
2024-04-07 00:03:46 -0400

    Initial commit

---
+++ App/config.py
@@ -0,0 +1,18 @@
+import os
+
+def load_config(app, overrides):
+    if os.path.exists(os.path.join('./App', 'custom_config.py')):
+        app.config.from_object('App.custom_config')
+    else:
+        app.config.from_object('App.default_config')
+    app.config.from_prefixed_env()
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['PREFERRED_URL_SCHEME'] = 'https'
+    app.config['UPLOADED_PHOTOS_DEST'] = "App/uploads"
+    app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token'
+    app.config["JWT_TOKEN_LOCATION"] = ["cookies", "headers"]
+    app.config["JWT_COOKIE_SECURE"] = True
+    app.config["JWT_COOKIE_CSRF_PROTECT"] = False
+    for key in overrides:
+        app.config[key] = overrides[key]
---
+++ App/main.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, render_template
+from flask_uploads import DOCUMENTS, IMAGES, TEXT, UploadSet, configure_uploads
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+
+from App.database import init_db
+from App.config import load_config
+
+from App.controllers import (
+    setup_jwt,
+    add_auth_context
+)
+
+from App.views import views
+
+def add_views(app):
+    for view in views:
+        app.register_blueprint(view)
+
+def create_app(overrides={}):
+    app = Flask(__name__, static_url_path='/static')
+    load_config(app, overrides)
+    CORS(app)
+    add_auth_context(app)
+    photos = UploadSet('photos', TEXT + DOCUMENTS + IMAGES)
+    configure_uploads(app, photos)
+    add_views(app)
+    init_db(app)
+    jwt = setup_jwt(app)
+    
+    @jwt.invalid_token_loader
+    @jwt.unauthorized_loader
+    def custom_unauthorized_response(error):
+        return render_template('401.html', error=error), 401
+    
+    app.app_context().push()
+    return app
+
