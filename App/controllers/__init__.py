78022fea500e4fc154badef683b2d744205bafc1
sagar-1503 <161993744+sagar-1503@users.noreply.github.com>
2024-04-22 04:19:27 -0400

    Reimplementing the App Folder

---
+++ App/controllers/__init__.py
@@ -0,0 +1,3 @@
+from .user import *
+from .auth import *
+from .initialize import *
---
+++ App/views/index.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
+from App.controllers import create_user, initialize
+
+index_views = Blueprint('index_views', __name__, template_folder='../templates')
+
+@index_views.route('/', methods=['GET'])
+def index_page():
+    return render_template('index.html')
+
+@index_views.route('/init', methods=['GET'])
+def init():
+    initialize()
+    return jsonify(message='db initialized!')
+
+@index_views.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({'status':'healthy'})
