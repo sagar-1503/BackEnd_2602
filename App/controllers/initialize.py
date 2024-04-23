da14c786573f7955d7b7e331c10bbf71cf083669
Ché Salazar <45917551+Supernedo@users.noreply.github.com>
2024-04-23 03:09:47 -0400

    Final initialize.py

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -10,9 +10,13 @@ import requests
 
 
 def initialize():
-    db.drop_all()
+    # db.drop_all()
     db.create_all()
-    create_user('BobTheBuilder', 'bob', 'bobpass')
+
+    user = User.query.filter_by(username="bob").first()
+
+    if not user:
+        create_user('BobTheBuilder', 'bob', 'bobpass')
 
     # Import movie files from API
 
@@ -25,77 +29,91 @@ def initialize():
     genre_response = requests.get(genre_url, headers=genre_headers)
     movie_genres = genre_response.json().get("genres", [])
 
-    for page in range(1,501):
-        url = "https://api.themoviedb.org/3/discover/movie"
-        params = {
-            "include_adult": "false",
-            "include_video": "true",
-            "language": "en-US",
-            "page": str(page),
-            "sort_by": "popularity.desc"
-        }
+    movie_test = Movie.query.first()
 
-        headers = {
-            "accept": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDcyMThmOTE2Yzk2MWEyNjc3ZmI1ZTdjMjFmZmNjNyIsInN1YiI6IjY2MThkM2VlMGYwZGE1MDE3Y2RmNWI3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MhyHjQ2gXlSLm0zjI5dEwX7nwQQ58hzd25wnCKC3fjo"
-        }
+    if movie_test:
+        print('database already initialized')
+    else:
+        # Initialize an empty list to store movie objects
+        movies_to_commit = []
 
-        response = requests.get(url, params=params, headers=headers)
-        movies = response.json().get('results', [])
+        for page in range(1, 501):
+            url = "https://api.themoviedb.org/3/discover/movie"
+            params = {
+                "include_adult": "false",
+                "include_video": "true",
+                "language": "en-US",
+                "page": str(page),
+                "sort_by": "popularity.desc"
+            }
 
-        for movie in movies:
-            existing_movie = Movie.query.filter_by(id=movie.get('id')).first()
-            if existing_movie:
-                # If a movie with the same ID already exists, skip adding it
-                continue
+            headers = {
+                "accept": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDcyMThmOTE2Yzk2MWEyNjc3ZmI1ZTdjMjFmZmNjNyIsInN1YiI6IjY2MThkM2VlMGYwZGE1MDE3Y2RmNWI3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MhyHjQ2gXlSLm0zjI5dEwX7nwQQ58hzd25wnCKC3fjo"
+            }
 
-            # Get genre names corresponding to the genre IDs
-            genre_names = [genre['name'] for genre in movie_genres if genre['id'] in movie.get('genre_ids', [])]
-            formatted_genre_names = ", ".join(genre_names)
+            response = requests.get(url, params=params, headers=headers)
+            movies = response.json().get('results', [])
 
-            new_movie = Movie(
-                # Set generic values if unable to attain a value
-                id=movie.get('id', 0),
-                title=movie.get('title', 'Movie_Title'), 
-                release_date=movie.get('release_date', 'Movie_Date'),
-                language=movie.get('original_language', 'Movie_Language'),
-                genres=formatted_genre_names,
-                description=movie.get('overview', 'Movie_Description'), 
-                thumbnail=movie.get('poster_path', 'Movie_Thumbnail_Link'), 
-                backdrop=movie.get('backdrop_path', 'Movie_Backdrop_Link'),
-                hasVideo=movie.get('video', False)
-            )
-            new_movie.href = new_movie.title.replace(" ", "_") + "_(film)" + "_" + str(new_movie.id)
+            for movie in movies:
+                existing_movie = Movie.query.filter_by(id=movie.get('id')).first()
+                if existing_movie:
+                    # If a movie with the same ID already exists, skip adding it
+                    continue
 
-            base_url = "https://image.tmdb.org/t/p/original"
-            poster_path = str(new_movie.thumbnail)
-            backdrop_path = str(new_movie.backdrop)
+                # Get genre names corresponding to the genre IDs
+                genre_names = [genre['name'] for genre in movie_genres if genre['id'] in movie.get('genre_ids', [])]
+                formatted_genre_names = ", ".join(genre_names)
 
-            new_movie.thumbnail = base_url + poster_path
-            new_movie.backdrop = base_url + backdrop_path
+                new_movie = Movie(
+                    # Set generic values if unable to attain a value
+                    id=movie.get('id', 0),
+                    title=movie.get('title', 'Movie_Title'), 
+                    release_date=movie.get('release_date', 'Movie_Date'),
+                    language=movie.get('original_language', 'Movie_Language'),
+                    genres=formatted_genre_names,
+                    description=movie.get('overview', 'Movie_Description'), 
+                    thumbnail=movie.get('poster_path', 'Movie_Thumbnail_Link'), 
+                    backdrop=movie.get('backdrop_path', 'Movie_Backdrop_Link'),
+                    hasVideo=movie.get('video', False)
+                )
+                new_movie.href = new_movie.title.replace(" ", "_") + "_(film)" + "_" + str(new_movie.id)
 
-            if (new_movie.has_video):
-                # Perform query for the particular movie
-                video_URL = "https://api.themoviedb.org/3/movie/" + str(new_movie.id) + "/videos?language=en-US"
+                base_url = "https://image.tmdb.org/t/p/original"
+                poster_path = str(new_movie.thumbnail)
+                backdrop_path = str(new_movie.backdrop)
 
-                headers = {
-                    "accept": "application/json",
-                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDcyMThmOTE2Yzk2MWEyNjc3ZmI1ZTdjMjFmZmNjNyIsInN1YiI6IjY2MThkM2VlMGYwZGE1MDE3Y2RmNWI3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MhyHjQ2gXlSLm0zjI5dEwX7nwQQ58hzd25wnCKC3fjo"
-                }
+                new_movie.thumbnail = base_url + poster_path
+                new_movie.backdrop = base_url + backdrop_path
 
-                video_response = requests.get(url, headers=headers)
-                videos = response.json().get('results', [])
+                if new_movie.has_video:
+                    # Perform query for the particular movie
+                    video_URL = "https://api.themoviedb.org/3/movie/" + str(new_movie.id) + "/videos?language=en-US"
 
-                # Take only the first response
-                new_movie.video_name = videos[0].get('name', "Video_Name")
+                    headers = {
+                        "accept": "application/json",
+                        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDcyMThmOTE2Yzk2MWEyNjc3ZmI1ZTdjMjFmZmNjNyIsInN1YiI6IjY2MThkM2VlMGYwZGE1MDE3Y2RmNWI3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MhyHjQ2gXlSLm0zjI5dEwX7nwQQ58hzd25wnCKC3fjo"
+                    }
 
-                site = videos[0].get('site', 'YouTube')
-                if (site == "YouTube"):
-                    new_movie.video_URL = "https://www.youtube.com/watch?v=" + videos[0].key
-                if (site == "Vimeo"):
-                    new_movie.video_URL = "https://vimeo.com/" + videos[0].key
-                if (site == "Dailymotion"):
-                    new_movie.video_URL = "https://www.dailymotion.com/video/" + videos[0].key
+                    video_response = requests.get(url, headers=headers)
+                    videos = response.json().get('results', [])
 
-            db.session.add(new_movie)
+                    # Take only the first response
+                    new_movie.video_name = videos[0].get('name', "Video_Name")
+
+                    site = videos[0].get('site', 'YouTube')
+                    if site == "YouTube":
+                        new_movie.video_URL = "https://www.youtube.com/watch?v=" + videos[0].key
+                    elif site == "Vimeo":
+                        new_movie.video_URL = "https://vimeo.com/" + videos[0].key
+                    elif site == "Dailymotion":
+                        new_movie.video_URL = "https://www.dailymotion.com/video/" + videos[0].key
+
+                # Add the new movie object to the list
+                movies_to_commit.append(new_movie)
+
+        # Commit all the movie objects to the database after the loop completes
+        db.session.add_all(movies_to_commit)
         db.session.commit()
+
+        print('database initialized')
