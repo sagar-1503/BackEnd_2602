6133946a503ae87ece3922c959fda5b45edf5c05
Ché Salazar <45917551+Supernedo@users.noreply.github.com>
2024-04-23 07:48:47 -0400

    Update default_config.py

--- App/default_config.py
+++ App/default_config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI="postgres://mydb_qz7t_user:N7fTmfbZttX1IOBLFLTLFoYtonStghGF@dpg-coj16gljm4es73a0dl20-a.oregon-postgres.render.com/mydb_qz7t"
+SQLALCHEMY_DATABASE_URI="sqlite:///temp-database.db"
 SECRET_KEY="secret key"
